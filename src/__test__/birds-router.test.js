b5e62abdf111ec9f52137526c8b2b72f5e9d1c0d
Noah Israel <noahmvf@gmail.com>
2018-07-07 16:21:39 -0700

    uuid changed to 3.3.2 in package

--- src/__test__/birds-router.test.js
+++ src/__test__/birds-router.test.js
@@ -1,112 +1,112 @@
-// 'use strict';
+'use strict';
 
-// const superagent = require('superagent');
-// const server = require('../lib/server');
-// const Bird = require('../model/birds');
+const superagent = require('superagent');
+const server = require('../lib/server');
+const Bird = require('../model/birds');
 
-// const apiUrl = 'http://localhost:5000/api/v1/birds';
+const apiUrl = 'http://localhost:5000/api/v1/birds';
 
-// const mockResource = {
-//   name: 'Mock Bird',
-//   habitat: 'Mock Habitat',
-//   region: 'Mock Region',
-// };
+const mockResource = {
+  name: 'Mock Bird',
+  habitat: 'Mock Habitat',
+  region: 'Mock Region',
+};
 
-// beforeAll(() => server.start(process.env.PORT || 3000));
-// afterAll(() => server.stop());
+beforeAll(() => server.start(process.env.PORT || 3000));
+afterAll(() => server.stop());
 
-// describe('POST to /api/v1/birds', () => {
-//   test('200 for successful saving of a new bird', () => {
-//     return superagent.post(apiUrl)
-//       .send(mockResource)
-//       .then((response) => {
-//         expect(response.body.name).toEqual(mockResource.name);
-//         expect(response.body.habitat).toEqual(mockResource.habitat);
-//         expect(response.body.region).toEqual(mockResource.region);
-//         expect(response.body._id).toBeTruthy();
-//         expect(response.status).toEqual(200);
-//       })
-//       .catch((err) => {
-//         throw err;
-//       });
-//   });
+describe('POST to /api/v1/birds', () => {
+  test('200 for successful saving of a new bird', () => {
+    return superagent.post(apiUrl)
+      .send(mockResource)
+      .then((response) => {
+        expect(response.body.name).toEqual(mockResource.name);
+        expect(response.body.habitat).toEqual(mockResource.habitat);
+        expect(response.body.region).toEqual(mockResource.region);
+        expect(response.body._id).toBeTruthy();
+        expect(response.status).toEqual(200);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
 
-//   test('404 NOT FOUND for a bad request', () => {
-//     return superagent.get(apiUrl)
-//       .send({})
-//       .then((response) => {
-//         throw response;
-//       })
-//       .catch((err) => {
-//         expect(err.status).toEqual(404);
-//         expect(err).toBeInstanceOf(Error);
-//       });
-//   });
-// });
+  test('404 NOT FOUND for a bad request', () => {
+    return superagent.get(apiUrl)
+      .send({})
+      .then((response) => {
+        throw response;
+      })
+      .catch((err) => {
+        expect(err.status).toEqual(404);
+        expect(err).toBeInstanceOf(Error);
+      });
+  });
+});
 
-// describe('GET /api/v1/birds', () => {
-//   let mockResourceForGet;
-//   beforeEach((done) => {
-//     const newBird = new Bird(mockResource);
-//     newBird.save()
-//       .then((bird) => {
-//         mockResourceForGet = bird;
-//         done();
-//       })
-//       .catch((err) => { 
-//         throw err;
-//       });
-//   });
+describe('GET /api/v1/birds', () => {
+  let mockResourceForGet;
+  beforeEach((done) => {
+    const newBird = new Bird(mockResource);
+    newBird.save()
+      .then((bird) => {
+        mockResourceForGet = bird;
+        done();
+      })
+      .catch((err) => { 
+        throw err;
+      });
+  });
 
-//   test('200 successful GET request', () => {
-//     console.log(apiUrl);
-//     return superagent.get(`${apiUrl}?id=${mockResourceForGet._id}`)
-//       .then((response) => {
-//         expect(response.status).toEqual(200);
-//         expect(response.body.name).toEqual(mockResourceForGet.name);
-//         expect(response.body.habitat).toEqual(mockResourceForGet.habitat);
-//         expect(response.body.region).toEqual(mockResourceForGet.region);
-//         expect(response.body._id).toBeTruthy();
-//       })
-//       .catch((err) => {
-//         throw err;
-//       });
-//   });
-// });
+  test('200 successful GET request', () => {
+    console.log(apiUrl);
+    return superagent.get(`${apiUrl}?id=${mockResourceForGet._id}`)
+      .then((response) => {
+        expect(response.status).toEqual(200);
+        expect(response.body.name).toEqual(mockResourceForGet.name);
+        expect(response.body.habitat).toEqual(mockResourceForGet.habitat);
+        expect(response.body.region).toEqual(mockResourceForGet.region);
+        expect(response.body._id).toBeTruthy();
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
+});
 
-// describe('DELETE /api/v1/birds', () => {
-//   let mockResourceForDelete;
+describe('DELETE /api/v1/birds', () => {
+  let mockResourceForDelete;
 
-//   beforeEach(() => {
-//     const newBird = new Bird(mockResource);
-//     return newBird.save()
-//       .then((bird) => {
-//         mockResourceForDelete = bird;
-//       })
-//       .catch((err) => {
-//         throw err;
-//       });
-//   });
+  beforeEach(() => {
+    const newBird = new Bird(mockResource);
+    return newBird.save()
+      .then((bird) => {
+        mockResourceForDelete = bird;
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
 
-//   test('200 on successful request', () => {
-//     return superagent.delete(`${apiUrl}?id=${mockResourceForDelete._id}`)
-//       .then((response) => {
-//         expect(response.status).toBe(204);
-//       })
-//       .catch((err) => {
-//         throw err;
-//       });
-//   });
+  test('200 on successful request', () => {
+    return superagent.delete(`${apiUrl}?id=${mockResourceForDelete._id}`)
+      .then((response) => {
+        expect(response.status).toBe(204);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
 
-//   test('404 for resource not found', () => {
-//     return superagent.delete((`${apiUrl}?id=00000`))
-//       .then((response) => {
-//         throw response;
-//       })
-//       .catch((err) => {
-//         expect(err.status).toEqual(404);
-//         expect(err).toBeInstanceOf(Error);
-//       });
-//   });
-// });
+  test('404 for resource not found', () => {
+    return superagent.delete((`${apiUrl}?id=00000`))
+      .then((response) => {
+        throw response;
+      })
+      .catch((err) => {
+        expect(err.status).toEqual(404);
+        expect(err).toBeInstanceOf(Error);
+      });
+  });
+});
 
