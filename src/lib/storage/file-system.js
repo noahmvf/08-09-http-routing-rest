dff35265075bcadbc4d3df4c749f7b9e450a998c
Noah Israel <noahmvf@gmail.com>
2018-07-07 15:24:25 -0700

    file-system

--- src/lib/storage/file-system.js
+++ src/lib/storage/file-system.js
@@ -6,13 +6,14 @@ const storage = module.exports = {};
 const dataDirectory = `${__dirname}/../../data`;
 
 storage.save = (schema, item) => {
-  const file = `${dataDirectory}/${schema}/${item._id}.json`; 
+  const file = `${dataDirectory}/${schema}/${item._id}.json`;
   const json = JSON.stringify(item);
+
   return new Promise((resolve, reject) => {
-    if (!schema) return reject(new Error('Cannot create a new item, schema required'));
-    if (!item || !item.name) return reject(new Error('Cannot create a new item, item and item.name required'));
+    if (!schema) return reject(new Error('Missing schema'));
+    if (!item || !item.nameEng) return reject(new Error('Missing item'));
+
     fs.writeFile(file, json, (err) => {
-      console.log(err, 'this is an fs writefile ERROR');
       if (err) return reject(err);
       return resolve(item);
     });
@@ -22,6 +23,7 @@ storage.save = (schema, item) => {
 
 storage.get = (schema, _id) => {
   const targetFile = `${dataDirectory}/${schema}/${_id}.json`;
+  
   return new Promise((resolve, reject) => {
     fs.readFile(targetFile, (err, data) => {
       if (err) return reject(err);
@@ -32,11 +34,11 @@ storage.get = (schema, _id) => {
 
 storage.delete = (schema, _id) => {
   const targetFile = `${dataDirectory}/${schema}/${_id}.json`;
+
   return new Promise((resolve, reject) => {
     fs.unlink(targetFile, (err) => {
       if (err) return reject(err);
-      return resolve(_id);
+      return resolve(targetFile);
     });
   });
 };
-
