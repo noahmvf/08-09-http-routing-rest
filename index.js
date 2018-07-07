34f9ad199bd9ca7e7c0113f20dd6acaaf279ee8a
Noah Israel <noahmvf@gmail.com>
2018-07-07 15:29:18 -0700

    index

--- index.js
+++ index.js
@@ -2,12 +2,4 @@
 
 require('dotenv').config();
 
-if (!process.env.NODE_ENV) {
-  throw new Error('Undefined NODE_ENV');
-}
-
-if (process.env.NODE_ENV !== 'production') {
-  require('babel-register');
-}
-
-require('./src/app');
+require('./src/app.js');
