41659de09923504f1b6b0c46561b0a347ae1c30b
Noah Israel <noahmvf@gmail.com>
2018-07-07 15:20:51 -0700

    router

--- src/lib/router.js
+++ src/lib/router.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const logger = require('./logger');
-const bodyParser = require('./body-parser');
-const customResponse = require('./response');
+const bodyParser = require('./body-parser.js');
+const customRes = require('./response.js');
 
 module.exports = class Router {
   constructor() {
@@ -25,30 +24,28 @@ module.exports = class Router {
   put(endpoint, callback) {
     this.routes.PUT[endpoint] = callback;
   }
+
   delete(endpoint, callback) {
     this.routes.DELETE[endpoint] = callback;
   }
 
   route() {
-    return (request, response) => {
-      Promise.all([bodyParser(request)]) 
+    return (req, res) => {
+      Promise.all([bodyParser(req)])
         .then(() => {
-          // [request.method] = 'GET' 'POST' 'PUT' 'DELETE'
-          const requestResponseCallback = this.routes[request.method][request.url.pathname];
-          const isFunction = typeof requestResponseCallback === 'function';
-          if (isFunction) return requestResponseCallback(request, response);
-          console.log(request, 'something something something');
-          
-          customResponse.sendError(response, 404, 'Route Not Registered');
+          const reqResCallback = this.routes[req.method][req.url.pathname];
+          const isFunction = typeof reqResCallback === 'function';
+          if (isFunction) return reqResCallback(req, res);
+
+          customRes.sendError(res, 404, 'Route Not Registered');
           return undefined;
         })
         .catch((err) => {
-          console.log(err, 'hey my name is error');
-          logger.log(logger.INFO, JSON.stringify(err));
-          customResponse.sendError(response, 404, 'Route Not Found');
+          console.log(err);
+
+          customRes.sendError(res, 404, 'Route Not Found');
           return undefined;
         });
     };
   }
 };
-
